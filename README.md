3e7e65d309e7b289a1e7b1a4d5bfcfbba4dfe6a0
Thomas Bollmeier <developer@thomas-bollmeier.de>
2024-04-14 10:21:34 +0200

    Add command-line arguments

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # boli-rust
-B(ollmeier's) O(wn) L(isp) I(mplementation) - My Toy Language in Rust
+
+(B)ollmeier's (O)wn (L)isp (I)mplementation - My Toy Language in Rust
--- boli/src/main.rs
+++ boli/src/main.rs
@@ -1,9 +1,40 @@
-use boli::frontend::parser::{visitor::JsonData, Parser};
+use boli::frontend::parser::{ast::Program, visitor::JsonData, ParseError, Parser as BoliParser};
+use clap::Parser;
+use std::{
+    fs::File,
+    io::{stdin, BufReader, Read, Result},
+};
 
-fn main() {
-    let parser = Parser::new();
-    let code = read_stdin();
+#[derive(Debug, Parser)]
+#[command(
+    author = clap::crate_authors!("\n"),
+    version = clap::crate_version!(),
+    about = "BOLI - (B)ollmeier's (O)wn (L)isp (I)nterpreter"
+)]
+struct Options {
+    #[arg(help = "Input file to parse", default_value = "-")]
+    input_file: String,
+
+    #[arg(short, long, help = "Show the AST after parsing")]
+    show_ast: bool,
+}
+
+fn main() -> Result<()> {
+    let options = Options::parse();
+
+    let code = read_input(options.input_file)?;
+
+    let parser = BoliParser::new();
     let parse_result = parser.parse(&code);
+
+    if options.show_ast {
+        show_ast(parse_result);
+    }
+
+    Ok(())
+}
+
+fn show_ast(parse_result: std::result::Result<Program, ParseError>) {
     if let Ok(ast) = parse_result {
         println!("{}", JsonData::from(ast));
     } else {
@@ -11,10 +42,15 @@ fn main() {
     }
 }
 
-fn read_stdin() -> String {
-    use std::io::{self, Read};
-
+fn read_input(file_path: String) -> Result<String> {
     let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer).unwrap();
-    buffer
+
+    if file_path == "-" {
+        stdin().read_to_string(&mut buffer)?;
+    } else {
+        let file = File::open(file_path)?;
+        let mut reader = BufReader::new(file);
+        reader.read_to_string(&mut buffer)?;
+    }
+    Ok(buffer)
 }
